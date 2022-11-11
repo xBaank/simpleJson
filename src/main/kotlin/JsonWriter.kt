64210a3613bdd02c81d56b93dfbc2fd2fd0e146f
Roberto Blázquez <elroby444@gmail.com>
2022-11-12 00:35:23 +0100

    Added dsl and writer

---
+++ src/main/kotlin/JsonWriter.kt
@@ -0,0 +1,62 @@
+import java.io.ByteArrayOutputStream
+import java.io.OutputStream
+import java.nio.charset.Charset
+
+class JsonWriter(stream: OutputStream, charset: Charset = Charsets.UTF_8) {
+    private val writer = stream.bufferedWriter(charset)
+
+        fun write(node: JsonNode) = when (node) {
+            is JsonArray -> writeArray(node)
+            is JsonObject -> writeObject(node)
+            is JsonString -> writeString(node)
+            is JsonNumber -> writeNumber(node)
+            is JsonBoolean -> writeBoolean(node)
+            JsonNull -> writeNull()
+        }.also { writer.flush() }
+
+        private fun writeArray(node: JsonArray) {
+            writer.write("[")
+            node.value.forEachIndexed { index, jsonNode ->
+                if (index != 0) writer.write(",")
+                write(jsonNode)
+            }
+            writer.write("]")
+        }
+
+        private fun writeObject(node: JsonObject) {
+            writer.write("{")
+            node.value.entries.forEachIndexed { index, (key, value) ->
+                if (index != 0) writer.write(",")
+                writeString(JsonString(key))
+                writer.write(":")
+                write(value)
+            }
+            writer.write("}")
+        }
+
+        private fun writeString(node: JsonString) {
+            writer.write("\"")
+            writer.write(node.value)
+            writer.write("\"")
+        }
+
+        private fun writeNumber(node: JsonNumber) {
+            writer.write(node.value.toString())
+        }
+
+        private fun writeBoolean(node: JsonBoolean) {
+            writer.write(node.value.toString())
+        }
+
+        private fun writeNull() {
+            writer.write("null")
+        }
+
+    companion object {
+        fun write(node: JsonNode) : String {
+            val stream = ByteArrayOutputStream()
+            JsonWriter(stream).write(node)
+            return stream.toString()
+        }
+    }
+}
---
+++ src/test/kotlin/JsonWriterTest.kt
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test
+
+class JsonWriterTest {
+    @Test
+    fun `should write json`() {
+        // TODO change builder to nodes?
+        val json = jObject {
+            "null" to null
+            "true" to true
+            "false" to false
+            "number" to 5.5
+            "string" to "string"
+            "array" to jArray {
+                add(1.2)
+                jObject {
+                    "string" to "string"
+                    "array" to jArray {
+                        add("first")
+                        add("second")
+                    }
+                }
+                add("string")
+                add(true)
+                add(false)
+                add(null)
+                add(jArray { add("first") })
+                add(jObject { "string" to "string" })
+            }
+            "object" to jObject {}
+        }
+
+        val string = JsonWriter.write(json)
+        assert(string == """{"null":null,"true":true,"false":false,"number":5.5,"string":"string","array":[1.2,{"string":"string","array":["first","second"]},"string",true,false,null,["first"],{"string":"string"},true],"object":{}}""")
+
+    }
+}
